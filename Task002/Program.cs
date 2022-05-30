d67e06185c009f3af8f1e7994bf4734edf1c1afe
Abylai <aba.omar@mail.ru>
2022-05-30 21:02:56 +0600

    Towers

---
+++ Task002/Program.cs
@@ -0,0 +1,11 @@
+﻿// Башня с блинами
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+{
+    if (count > 1) Towers(with, on, some, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(with, on, some, count - 1);
+}
+
+
+Towers();
